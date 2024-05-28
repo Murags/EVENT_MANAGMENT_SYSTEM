6885ebbca3f1eb431ca46fa4bf68e6ec0ddbaeb6
JustRelaxWorld <austin.kimathi@strathmore.edu>
2024-05-28 13:43:13 +0300

    Latest GUIs

---
+++ src/eventmanagement/CustomerHomepage.java
@@ -0,0 +1,16 @@
+package eventmanagement;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CustomerHomepage extends JFrame
+{
+    CustomerHomepage()
+    {
+        this.setSize(500,500);
+        this.setLayout(null);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.
+    }
+}
